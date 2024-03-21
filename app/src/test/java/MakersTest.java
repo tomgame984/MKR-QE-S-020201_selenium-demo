2922eca6a78cef1b738fcbb1ac36d98707900035
Tom <tomgame984@gmail.com>
2024-03-21 11:37:52 +0000

    Challenge Complete

--- app/src/test/java/MakersTest.java
+++ app/src/test/java/MakersTest.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class MakersTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class MakersTest {
+    private static FirefoxDriver driver;
+    @BeforeAll
+    static void launchBrowser() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+    }
+    @Test
+    void shouldPrintPageTitle() {
+        driver.get("https://makers.tech");
+        System.out.println(driver.getTitle());
+    }
+    @AfterAll
+//    Run once, at the end of the test run.
+    static void closerBrowser() {
+        driver.quit();
+    }
+
 }
